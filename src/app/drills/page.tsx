26a05af054cee8f66bff562b51daf37e6c7d564a
Jason Boyer <phronesis700@gmail.com>
2025-10-11 18:44:33 -0500

    feat: add icons to drill modal Skills, Equipment, and Duration labels

--- src/app/drills/page.tsx
+++ src/app/drills/page.tsx
@@ -273,7 +273,7 @@ export default function DrillsPage() {
                       fill="currentColor"
                       viewBox="0 0 20 20"
                     >
-                      <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                     </svg>
                   </div>
                   <div>
@@ -292,11 +292,7 @@ export default function DrillsPage() {
                       fill="currentColor"
                       viewBox="0 0 20 20"
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                        clipRule="evenodd"
-                      />
+                      <path d="M8 2a1 1 0 000 2h2a1 1 0 100-2H8zM3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM2 10a8 8 0 1116 0v6a2 2 0 01-2 2H4a2 2 0 01-2-2v-6zm8 1c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2z" />
                     </svg>
                   </div>
                   <div>
@@ -315,11 +311,7 @@ export default function DrillsPage() {
                       fill="currentColor"
                       viewBox="0 0 20 20"
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                        clipRule="evenodd"
-                      />
+                      <path d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12zm1-7a1 1 0 10-2 0v3a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V9z" />
                     </svg>
                   </div>
                   <div>
